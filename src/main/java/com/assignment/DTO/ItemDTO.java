28b5b3d57c2d7c3e185d01bf2c0a1dcb70dbf222
Bijay Shrestha <isongum@gmail.com>
2024-08-31 10:19:01 +0545

    Added Validation

--- src/main/java/com/assignment/DTO/ItemDTO.java
+++ src/main/java/com/assignment/DTO/ItemDTO.java
@@ -1,26 +1,28 @@
 package com.assignment.DTO;
 
-
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class ItemDTO {
 
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
     private String name;
 
+    @NotBlank(message = "Unique code is mandatory")
     private String uniqueCode;
 
+    @NotNull(message = "Quantity is mandatory")
+    @Min(value = 1, message = "Quantity must be at least 1")
     private Integer quantity;
 
     private String packingType;
 
+    @Min(value = 1, message = "Pack quantity must be at least 1")
     private Integer packQuantity;
 
-    public ItemDTO(String name, String uniqueCode, Integer quantity, String packingType, Integer packQuantity) {
-        this.name = name;
-        this.uniqueCode = uniqueCode;
-        this.quantity = quantity;
-        this.packingType = packingType;
-        this.packQuantity = packQuantity;
-    }
 }
---
+++ src/main/java/com/assignment/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.assignment.Exception;
+
+import com.assignment.Utils.ResponseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ResponseDTO> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        log.error("MethodArgumentNotValidException : {}", errors);
+        ResponseDTO response = ResponseDTO.customResponseDTO(
+                HttpStatus.BAD_REQUEST,
+                "One or more fields missing or null",
+                errors);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
